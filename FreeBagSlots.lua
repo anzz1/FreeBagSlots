ae84a389504332982084de719e53041fc1c8a9ed
anzz1 <anzz1@live.com>
2019-06-04 17:43:23 +0300

    add localizations support (enUS/deDE/zhTW/zhCN/koKR/esES/ruRU)

--- FreeBagSlots.lua
+++ FreeBagSlots.lua
@@ -6,15 +6,13 @@
 ---------------------------
 
 
--- TODO: localizations, currently only works on enUS client
--- itemType and subType are localized, so a list of what GetItemInfo() returns in every language would be needed
 local function isRegularBag(bag)
 	if( bag < 0 ) then return false end
 	if( bag == 0 ) then return true end -- bag 0 = backpack
 	local _, _, id = strfind(GetInventoryItemLink("player", ContainerIDToInventoryID(bag)) or "", "item:(%d+)")
 	if id then
 		local _, _, _, _, _, subType = GetItemInfo(id)
-		return (subType == "Bag" or false)
+		return (subType == "Bag" or subType == "Beh\195\164lter" or subType == "Sac" or subType == "容器" or subType == "背包" or subType == "가방" or subType == "Bolsa" or subType == "Сумка" or false)
 	end
 	return false
 end
@@ -48,15 +46,11 @@ end
 
 ---------------------------------------------------
 do
-	if (GetLocale() == "enUS" or GetLocale() == "enGB") then
-		local f = CreateFrame('Frame')
-		f:RegisterEvent('PLAYER_LOGIN')
-		f:RegisterEvent('BAG_UPDATE')
-		f:SetScript('OnEvent', function()
-			MainMenuBarBackpackButton_UpdateFreeSlots()
-		end)
-	else
-		DEFAULT_CHAT_FRAME:AddMessage("|c000066FFFreeBagSlots:|c00FF0000 Could not load addon. Currently only enUS locale is supported :(")
-	end
+	local f = CreateFrame('Frame')
+	f:RegisterEvent('PLAYER_LOGIN')
+	f:RegisterEvent('BAG_UPDATE')
+	f:SetScript('OnEvent', function()
+		MainMenuBarBackpackButton_UpdateFreeSlots()
+	end)
 end
 ---------------------------------------------------
